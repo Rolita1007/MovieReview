d893dc8024bf40db0b0da64c8ef4ce61e2f297ae
rolita1007 <rolita1007@gmail.com>
2017-12-15 22:57:58 -0500

    created the simple_form to the new path

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,10 +3,18 @@ class MoviesController < ApplicationController
   def index
   end
     
-  def new
+	def new
+		@movie = Movie.new
   end
 
-  def create
-  end
+	def create
+		@movie = Movie.new(book_params)
+	end
+	
+	private
+	
+		def book_params
+			params.require(:movie).permit(:title, :discription, :director)
+		end
 
 end
